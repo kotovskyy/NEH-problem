251ea5500eb6bc9f5c4254d3e4ab9c7c166eaa12
acodered <alexkondratru@gmail.com>
2024-04-10 14:38:45 +0200

    Make readData() create Task objects

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from typing import List
 
+
+class Task:
+    def __init__(self, id: int, tpm: List[int]) -> None:
+        self.id = id
+        self.tpm = tpm
+    
+    def __repr__(self) -> str:
+        return f"id: {self.id} | {[time for time in self.tpm]}"
+
+
 def readData(filepath: str) -> dict[str: List[str]]:
     """
     Reads data from a file with sections defined by "data.XXX" lines.
@@ -12,6 +22,7 @@ def readData(filepath: str) -> dict[str: List[str]]:
     """
     data = {}
     current_section = None
+    counter = 0
     with open(filepath, 'r') as f:
         saveData = False
         for line in f:
@@ -22,12 +33,19 @@ def readData(filepath: str) -> dict[str: List[str]]:
 
             if line.startswith("data."):
                 saveData = True
+                counter = 0
                 current_section = line[:-1]
                 data[current_section] = []
             else:
                 if current_section and saveData:
-                    data[current_section].append(line)
+                    if counter == 0:
+                        counter += 1    
+                        continue
+                    newTask = Task(counter, [int(item) for item in line.split(" ")])
+                    data[current_section].append(newTask)
+                    counter += 1 
     return data
+
 
 def main():
     data = readData("data/data.txt")
