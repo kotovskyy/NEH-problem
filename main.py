1c0e8b82257f7842b39e77249aa7a8b194138456
acodered <alexkondratru@gmail.com>
2024-04-12 00:20:24 +0200

    Add QNEH algorithm

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ class Task:
         return f"id: {self.id} | {[time for time in self.tpm]}"
 
 
+def calculate_time(func):
+    """
+        Decorator to calculate total execution time of a function.
+    """
+    def inner(*args, **kwargs):
+        import time
+        start = time.time()
+        order = func(*args, **kwargs)
+        end = time.time()
+        totalTime = end - start
+        print(f"Execution time: {totalTime:.3} s")
+        return order
+        
+    return inner
+
+
 def readData(filepath: str) -> dict[str: List[Task]]:
     """
     Reads data from a file with sections defined by "data.XXX" lines.
@@ -61,25 +77,64 @@ def getTotalTime(data):
             machine_free_at[m] = Cmax
     return Cmax
 
+def _createCmaxTable(data, N, M, backward=False):
+    prev = np.zeros(M)
+    output = np.zeros((M, N))
+    if backward:
+        for i in range(N-1, -1, -1):
+            t = 0
+            for m in range(M-1, -1, -1):
+                task_time = data[i].tpm[m]
+                Cmax = max(prev[m], t) + task_time
+                t = Cmax
+                prev[m] = Cmax
+                output[m, i] = Cmax
+    else:
+        for i, task in enumerate(data):
+            t = 0
+            for m in range(M):
+                task_time = task.tpm[m]
+                Cmax = max(prev[m], t) + task_time
+                t = Cmax
+                prev[m] = Cmax
+                output[m, i] = Cmax
+    return output
+
+def QNEHTotalTime(task, forward, backward, N, M, i):
+    t = 0
+    task_table = np.zeros((M, 1))
+    for m in range(M):
+        task_time = task.tpm[m]
+        Cmax = max(forward[m, i-1], t) + task_time if i > 0 else t + task_time
+        t = Cmax
+        task_table[m, 0] = Cmax
+    result = np.hstack((forward[:, :i], task_table, backward[:, i:]))
+    
+    return max(result[:, i] + result[:, i+1]) if i < N else max(result[:, i])
+
+
 def _getTasksTotals(data):
     return sorted([[i, sum(task.tpm)] for i, task in enumerate(data)], key=lambda x: x[1], reverse=True)
 
-def calculate_time(func):
-    """
-        Decorator to calculate total execution time of a function.
-    """
-    def inner(*args, **kwargs):
-        import time
-        start = time.time()
-        order = func(*args, **kwargs)
-        end = time.time()
-        totalTime = end - start
-        print(f"Execution time: {totalTime:.3} s")
-        return order
-        
-    return inner
 
-
+@calculate_time
+def QNEH(data):
+    data = np.asarray(data)
+    task_totals = _getTasksTotals(data)
+    order, index = [], None
+    N, M = len(order), len(data[0].tpm)
+    
+    for t in task_totals:
+        C_max = math.inf
+        forward = _createCmaxTable(data[order], N, M, False)
+        backward = _createCmaxTable(data[order], N, M, True)
+        for i in range(N+1):
+            c = QNEHTotalTime(data[t[0]], forward, backward, N, M, i)
+            if c < C_max:
+                C_max, index = c, i
+        order.insert(index, t[0])
+    return order
+    
 @calculate_time
 def NEH(data):
     data = np.asarray(data)
@@ -101,18 +156,19 @@ def printOrder(order):
     print("Order: " + " ".join([str(i+1) for i in order]))
 
 
-def testSolution(data, datasetName: str) -> None:
+def testSolution(data, datasetName: str, func) -> None:
     data = data[datasetName]
     print(f"DATASET : {datasetName}")
-    order = NEH(data)
+    order = func(data)
     print(f"Min Cmax: {getTotalTime(np.asarray(data)[order])}")
     printOrder(order)
 
 
 def main():
-    dataName = "data.001"
+    dataName = "data.120"
     data = readData("data/data.txt")
-    testSolution(data, dataName)
+    testSolution(data, dataName, QNEH)
+ 
     
 if __name__ == "__main__":
     main()
