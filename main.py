dcf00201eebfbae179f422e5cf33f82945f13889
acodered <alexkondratru@gmail.com>
2024-04-15 22:59:36 +0200

    Optimize and refactor code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import List
 import numpy as np
 import math
 
+
 class Task:
     def __init__(self, id: int, tpm: List[int]) -> None:
         self.id = id
@@ -65,89 +66,92 @@ def readData(filepath: str) -> dict[str: List[Task]]:
 
 
 def getTotalTime(data):
-    N_machines = len(data[0].tpm)
+    M = len(data[0].tpm)
+    machine_time = np.zeros(M)
     Cmax = 0
-    machine_free_at = [0] * N_machines
     for task in data:
-        t = 0
-        for m in range(N_machines):
-            task_time = task.tpm[m]
-            Cmax = max(machine_free_at[m], t) + task_time
-            t = Cmax
-            machine_free_at[m] = Cmax
-    return Cmax
+        task_frees_at = 0
+        for m in range(M):
+            entry_time = max(machine_time[m], task_frees_at)
+            exit_time = entry_time + task.tpm[m]
+            machine_time[m] = exit_time
+            Cmax = task_frees_at = exit_time
+    return int(Cmax)
 
-def _createCmaxTable(data, N, M, backward=False):
+
+def _createCmaxTable(data, N, M):
+    forward = np.zeros((M, N))
+    backward = np.zeros((M, N))
+
     prev = np.zeros(M)
-    output = np.zeros((M, N))
-    if backward:
-        for i in range(N-1, -1, -1):
-            t = 0
-            for m in range(M-1, -1, -1):
-                task_time = data[i].tpm[m]
-                Cmax = max(prev[m], t) + task_time
-                t = Cmax
-                prev[m] = Cmax
-                output[m, i] = Cmax
-    else:
-        for i, task in enumerate(data):
-            t = 0
-            for m in range(M):
-                task_time = task.tpm[m]
-                Cmax = max(prev[m], t) + task_time
-                t = Cmax
-                prev[m] = Cmax
-                output[m, i] = Cmax
-    return output
+    for i in range(N):
+        t = 0
+        task_times = data[i].tpm
+        for m in range(M):
+            task_time = task_times[m]
+            t = max(prev[m], t) + task_time
+            prev[m] = forward[m, i] = t
+
+    prev = np.zeros(M)
+    for i in range(N-1, -1, -1):
+        t = 0
+        task_times = data[i].tpm
+        for m in range(M-1, -1, -1):
+            task_time = task_times[m]
+            t = max(prev[m], t) + task_time
+            prev[m] = backward[m, i] = t
+
+    return forward, backward
+
 
 def QNEHTotalTime(task, forward, backward, N, M, i):
     t = 0
     task_table = np.zeros((M, 1))
+    machine_time = forward[:, i-1] if i > 0 else np.zeros(M)
     for m in range(M):
         task_time = task.tpm[m]
-        Cmax = max(forward[m, i-1], t) + task_time if i > 0 else t + task_time
-        t = Cmax
-        task_table[m, 0] = Cmax
-    result = np.hstack((forward[:, :i], task_table, backward[:, i:]))
-    
-    return np.max(result[:, i] + result[:, i+1]) if i < N else np.max(result[:, i])
+        t = max(machine_time[m], t) + task_time
+        task_table[m, 0] = t
+        
+    if i < N:
+        return np.max(task_table[:, 0] + backward[:, i])
+    return t
 
 
-def _getTasksTotals(data):
-    return sorted([[i, sum(task.tpm)] for i, task in enumerate(data)], key=lambda x: x[1], reverse=True)
+def _getTaskInputOrder(data):
+    return [t.id-1 for t in sorted(data, key=lambda x: np.sum(x.tpm), reverse=True)]
 
 
 @calculate_time
 def QNEH(data):
     data = np.asarray(data)
-    task_totals = _getTasksTotals(data)
+    input_order = _getTaskInputOrder(data)
     order, index = [], None
     M = len(data[0].tpm)
-    for t in task_totals:
+    for t_index in input_order:
         N = len(order) 
         C_max = math.inf
-        forward = _createCmaxTable(data[order], N, M, False)
-        backward = _createCmaxTable(data[order], N, M, True)
+        forward, backward = _createCmaxTable(data[order], N, M)
         for i in range(N+1):
-            c = QNEHTotalTime(data[t[0]], forward, backward, N, M, i)
+            c = QNEHTotalTime(data[t_index], forward, backward, N, M, i)
             if c < C_max:
                 C_max, index = c, i
-        order.insert(index, t[0])
+        order.insert(index, t_index)
     return order
     
 @calculate_time
 def NEH(data):
-    task_totals = _getTasksTotals(data)
+    task_totals = _getTaskInputOrder(data)
     order, index = [], None
     
-    for t in task_totals:
+    for t_index in task_totals:
         C_max = math.inf
         for i in range(len(order)+1):
-            new_order = order[:i] + [t[0]] + order[i:]
+            new_order = order[:i] + [t_index] + order[i:]
             c = getTotalTime(data[new_order])
             if c < C_max:
                 C_max, index = c, i
-        order.insert(index, t[0])
+        order.insert(index, t_index)
     return order
 
 
@@ -167,7 +171,14 @@ def main():
     dataName = "data.120"
     data = readData("data/data.txt")
     testSolution(data, dataName, QNEH)
- 
+    # data = np.array([Task(1, [1, 1, 3]),
+    #                  Task(2, [3, 4, 3]),
+    #                  Task(3, [4, 1, 2]),
+    #                  Task(4, [2, 4, 1])])
+    # print(_createCmaxTable(data, 4, 3))
+    # order = QNEH(data)
+    # print(f"Total time: {getTotalTime(np.asarray(data)[order])}")
+    # printOrder(order)
     
 if __name__ == "__main__":
     main()
